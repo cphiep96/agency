2a9f0a5ba896ecdd91e5b297f0c99b38f7a37950
cphiep96 <cphiep96@gmail.com>
2025-08-08 21:11:09 +0700

    Update contact address in `page-lien-he.php` for accuracy
    - Corrected the address details to reflect the accurate location in the contact page, ensuring users have the correct information.

--- page-lien-he.php
+++ page-lien-he.php
@@ -17,7 +17,7 @@ get_header(); ?>
           <div class="grid grid-cols-1 md:grid-cols-1 gap-8">
             <div class="text-center">
                 <h2 class="text-2xl font-semibold mb-4">Thông Tin Liên Hệ</h2>
-                <p class="mb-4"><strong>Địa chỉ:</strong> Đ. Nguyễn Văn Công, Phường Hạnh Thông, Hồ Chí Minh 100000</p>
+                <p class="mb-4"><strong>Địa chỉ:</strong> 489 Đ. Nguyễn Văn Công, Phường Hạnh Thông, Hồ Chí Minh 100000</p>
                 <p class="mb-4"><strong>Địa chỉ:</strong> Tổ 1, khu vực 1, phường Quy Nhơn Nam, tỉnh Gia Lai</p>
                 <p class="mb-4"><strong>Hotline:</strong> <a href="tel:+8439669350489" class="text-orange-300">0396693505</a></p>
                 <p class="mb-4"><strong>Zalo:</strong> <a href="https://zalo.me/0396693505" class="text-orange-300">0396693505</a></p>
